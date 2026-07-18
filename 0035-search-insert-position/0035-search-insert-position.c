a94ae5cc08ab6acc21b3bc8976590f09e6e4d6e1
bweb2552 <gamer42k65@gmail.com>
2026-07-18 10:10:31 +0530

    Time: 0 ms (100%), Space: 8.9 MB (70.61%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -0,0 +1,16 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int low=0;int high=numsSize-1;int mid;
+    while(low<=high){
+        mid=(high+low)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(nums[mid]<target){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return low;
+}
